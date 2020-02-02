d8988447028de4c938cdf3a761750f3ec4a68cb5
felipendc <felipendc10@gmail.com>
2020-02-02 19:38:42 -0300

    important information!

--- scripts/vicyos-update-system.sh
+++ scripts/vicyos-update-system.sh
@@ -8,7 +8,7 @@
 # If your Linux Distribution has a different terminal,
 # replace "urxvt" with the name of your terminal.
 
+# If you are running a Linux Distribution based on Ubuntu or Debian, 
+# you will need to change: "sudo pacman -Syu" to "sudo apt-get update && sudo apt-get upgrade".
+
 urxvt -e sh -c "sudo pacman -Syu"
-
-
-
